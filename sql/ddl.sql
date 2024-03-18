f1321908020fbb1c210eb1939f6189f362b8ead5
Michael Ripa <m.ripa123@gmail.com>
2024-03-18 19:07:04 -0400

    Moved sql files into a seperate directory

---
+++ sql/ddl.sql
@@ -0,0 +1,8 @@
+-- Students Table
+CREATE TABLE students (
+    student_id SERIAL PRIMARY KEY,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    email TEXT NOT NULL UNIQUE,
+    enrollment_date DATE
+);
